5b51b19fa02683895c2a16df34d2f91f2351a300
Pramoth Ragavan <107881923+pramothragavan@users.noreply.github.com>
2025-03-17 12:27:24 +0000

    working with inconsistent results

--- src/aisemirings.c
+++ src/aisemirings.c
@@ -29,9 +29,65 @@ Obj FuncIsLeftRightDistributive(Obj self, Obj A, Obj M) {
   return True;
 }
 
+typedef union {
+  const UInt2 *ptr2;
+  const UInt4 *ptr4;
+} PermAddr;
+
+static inline int GetImage(int element, const PermAddr addr, int deg, int type) {
+  return (type == T_PERM2) ?
+      IMAGE(element - 1, addr.ptr2, deg) + 1 :
+      IMAGE(element - 1, addr.ptr4, deg) + 1;
+}
+
+void FuncPermuteMultiplicationTable(Obj self, Obj temp, Obj M, Obj p) {
+  int n = LEN_LIST(M);
+  int deg_p, deg_q;
+  PermAddr addr_p, addr_q;
+  int p_type, q_type;
+  
+  p_type = TNUM_OBJ(p);
+  if (p_type == T_PERM2) {
+      addr_p.ptr2 = CONST_ADDR_PERM2(p);
+      deg_p = DEG_PERM2(p);
+  } else {
+      addr_p.ptr4 = CONST_ADDR_PERM4(p);
+      deg_p = DEG_PERM4(p);
+  }
+  
+  Obj q = STOREDINV_PERM(p);
+  if (q == 0) {
+      q = INV(p);
+      SET_STOREDINV_PERM(p, q);
+  }
+
+  q_type = TNUM_OBJ(q);
+  if (q_type == T_PERM2) {
+      addr_q.ptr2 = CONST_ADDR_PERM2(q);
+      deg_q = DEG_PERM2(q);
+  } else {
+      addr_q.ptr4 = CONST_ADDR_PERM4(q);
+      deg_q = DEG_PERM4(q);
+  }
+
+  for (int i = 1; i <= n; i++) {
+    Obj row = ELM_LIST(temp, i);
+    int ii = GetImage(i, addr_q, deg_q, q_type);
+
+    for (int j = 1; j <= n; j++) {
+      int home = Int_ObjInt(ELM_LIST(ELM_LIST(M, ii), GetImage(j, addr_q, deg_q, q_type)));
+      int new_val = GetImage(home, addr_p, deg_p, p_type);
+
+      ASS_LIST(row, j, INTOBJ_INT(new_val));
+    }
+  }
+  CHANGED_BAG(temp);
+}
+
 // Table of functions to export
 static StructGVarFunc GVarFuncs[] = {
     GVAR_FUNC(IsLeftRightDistributive, 2, "A, M"),
+    GVAR_FUNC(PermuteMultiplicationTable, 3, "temp, M, p"),
     {0} /* Finish with an empty entry */
 };
 
