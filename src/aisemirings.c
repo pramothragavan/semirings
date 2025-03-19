1ef68809e5aa996e1bf3e0195674f09d6e29724a
Pramoth Ragavan <107881923+pramothragavan@users.noreply.github.com>
2025-03-19 17:04:49 +0000

    refactor

--- src/aisemirings.c
+++ src/aisemirings.c
@@ -29,63 +29,47 @@ Obj FuncIsLeftRightDistributive(Obj self, Obj A, Obj M) {
   return True;
 }
 
-typedef union {
-  const UInt2 *ptr2;
-  const UInt4 *ptr4;
-} PermAddr;
-
-static inline int GetImage(int element, const PermAddr addr, int deg, int type) {
-  return (type == T_PERM2) ?
-      IMAGE(element - 1, addr.ptr2, deg) + 1 :
-      IMAGE(element - 1, addr.ptr4, deg) + 1;
-}
-
-static inline PermAddr GetAddr(Obj perm, int perm_type) {
-  PermAddr pa;
-  if (perm_type == T_PERM2) {
-      pa.ptr2 = CONST_ADDR_PERM2(perm);
-  } else {
-      pa.ptr4 = CONST_ADDR_PERM4(perm);
-  }
-  return pa;
-}
-
 void FuncPermuteMultiplicationTable(Obj self, Obj temp, Obj M, Obj p) {
   int n = LEN_LIST(M);
 
   int p_type = TNUM_OBJ(p);
-  int deg_p;
   if (p_type == T_PERM2) {
-      deg_p = DEG_PERM2(p);
-  } else {
-      deg_p = DEG_PERM4(p);
-  }
-
-  Obj q = STOREDINV_PERM(p);
-  if (q == 0) {
-      q = INV(p);
-      SET_STOREDINV_PERM(p, q);
-  }
-
-  int q_type = TNUM_OBJ(q);
-  int deg_q;
-  if (q_type == T_PERM2) {
-      deg_q = DEG_PERM2(q);
-  } else {
-      deg_q = DEG_PERM4(q);
-  }
-  
-  for (int i = 1; i <= n; i++) {
-    Obj row = ELM_LIST(temp, i);
-    int ii = GetImage(i, GetAddr(q, q_type), deg_q, q_type);
-
-    for (int j = 1; j <= n; j++) {
-      int home = Int_ObjInt(ELM_LIST(ELM_LIST(M, ii), GetImage(j, GetAddr(q, q_type), deg_q, q_type)));
-      int new_val = GetImage(home, GetAddr(p, p_type), deg_p, p_type);
-
-      ASS_LIST(row, j, INTOBJ_INT(new_val));
+      int deg_p = DEG_PERM2(p);
+      Obj q = STOREDINV_PERM(p);
+      if (q == 0) {
+          q = INV(p);
+          SET_STOREDINV_PERM(p, q);
+        }
+      int deg_q = DEG_PERM2(q);
+      for (int i = 1; i <= n; i++) {
+        Obj row = ELM_LIST(temp, i);
+        int ii = IMAGE(i - 1, CONST_ADDR_PERM2(q), deg_q) + 1;
+    
+        for (int j = 1; j <= n; j++) {
+          int home = Int_ObjInt(ELM_LIST(ELM_LIST(M, ii), IMAGE(j - 1, CONST_ADDR_PERM2(q), deg_q) + 1));
+          int new_val = IMAGE(home - 1, CONST_ADDR_PERM2(p), deg_p) + 1;
+          ASS_LIST(row, j, INTOBJ_INT(new_val));
+        }
+      }
+    } else {
+      int deg_p = DEG_PERM4(p);
+      Obj q = STOREDINV_PERM(p);
+      if (q == 0) {
+          q = INV(p);
+          SET_STOREDINV_PERM(p, q);
+        }
+      int deg_q = DEG_PERM4(q);
+      for (int i = 1; i <= n; i++) {
+        Obj row = ELM_LIST(temp, i);
+        int ii = IMAGE(i - 1, CONST_ADDR_PERM4(q), deg_q) + 1;
+    
+        for (int j = 1; j <= n; j++) {
+          int home = Int_ObjInt(ELM_LIST(ELM_LIST(M, ii), IMAGE(j - 1, CONST_ADDR_PERM4(q), deg_q) + 1));
+          int new_val = IMAGE(home - 1, CONST_ADDR_PERM4(p), deg_p) + 1;
+          ASS_LIST(row, j, INTOBJ_INT(new_val));
+        }
+      }
     }
-  }
   CHANGED_BAG(temp);
 }
 
