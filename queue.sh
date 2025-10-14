8c83b4a734feac36c93b73df3cb27259761c2e35
Joseph Edwards <josephdavidedwards@gmail.com>
2025-10-14 17:53:39 +0100

    add some logs

--- queue.sh
+++ queue.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-./parallel.sh "NrSemiringsWithOne(7, true)" | tee gen_numbers_2/semi-1-anti.txt
-./parallel.sh "NrSemiringsWithOne(7, false)" | tee gen_numbers_2/semi-1-iso.txt
-./parallel.sh "NrSemiringsWithZero(7, true)" | tee gen_numbers_2/semi-0-anti.txt
-./parallel.sh "NrSemiringsWithZero(7, false)" | tee gen_numbers_2/semi-0-iso.txt
-./parallel.sh "NrSemirings(7, true)" | tee gen_numbers_2/semi-anti.txt
-./parallel.sh "NrSemirings(7, false)" | tee gen_numbers_2/semi-iso.txt
+./parallel.sh "NrCommutativeSemiringsWithOneAndZero(8, true)" | tee gen_numbers_5/comm-0-1.txt
+./parallel.sh "NrSemiringsWithOneAndZero(8, true)" | tee gen_numbers_5/semi-iso-anti-0-1.text
+./parallel.sh "NrSemiringsWithOneAndZero(8, false)" | tee gen_numbers_5/semi-iso-0-1.text
+./parallel.sh "NrCommutativeAiSemiringsWithOneAndZero(9, true)" | tee gen_numbers_5/comm-ai-0-1.txt
